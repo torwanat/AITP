695b958f9355177598e8699abac66ccf88d84efd
torwanat <nawrotm37@gmail.com>
2025-05-29 13:48:54 +0200

    add more solutions

---
+++ djikstra.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<cstdlib>
+#define MAXN 10000
+#define INF 1000000000
+
+using namespace std;
+
+int n, m;
+vector<int> kraw[MAXN + 1];
+vector<int> waga[MAXN + 1];
+int dis[MAXN + 1];
+int ojc[MAXN + 1];
+int vis[MAXN + 1];
+
+int main(){
+    cout << "Podaj ilosc wierzcholkow i "
+}
---
+++ razraz.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int l;
+	cin>>l;
+	string words[1000];
+	cin>>words[0];
+	cout<<words[0][2];
+}
